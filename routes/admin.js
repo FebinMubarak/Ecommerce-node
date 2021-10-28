c94e4e9d05800ac6df16bffc90957ae12617f0ea
Febin <febinmubarak333@gmail.com>
2021-10-28 07:50:46 +0400

    table added for adding products

--- routes/admin.js
+++ routes/admin.js
@@ -31,5 +31,12 @@ router.get('/', function(req, res, next) {
   ]
   res.render("admin/all-products",{flowers,admin:true})
 });
+router.get("/add-product",function(req,res){
+  res.render("admin/add-product")
+})
+router.post("/add-product",function(req,res){
+  console.log(req.body)
+  console.log(req.files.Image)
+})
 
 module.exports = router;
